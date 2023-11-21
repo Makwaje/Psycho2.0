6bc5c25d569064d115315af41b353e97d083945c
makwaje <alloushi2010@gmail.com>
2023-11-21 20:56:26 +0200

    todo app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,191 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 
-export default function Home() {
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { clear, log } from "console";
+import { Todo } from "./model";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Check, PenIcon, Trash2 } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { Textarea } from "@/components/ui/textarea";
+
+// export default async function App(): React.FC {
+//   return <main></main>;
+// }
+
+function App(): React.ReactNode {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleAdd(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  }
+
+  console.log(todos);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className=" max-w-6xl">
+      <h1 className="text-center mt-16">Task App</h1>
+      <InputWithLabel onClick={handleAdd} todo={todo} setTodo={setTodo} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* <div className="grid grid-cols-2 gap-y-8 max-w-2xl mx-auto"> */}
+      <div className="flex justify-evenly flex-wrap gap-8">
+        {todos.map((todo) => (
+          <ToDoCard
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+            key={todo.id}
+          />
+        ))}
       </div>
     </main>
-  )
+  );
+}
+
+export default App;
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  onClick: (e: React.FormEvent) => void;
+}
+
+function InputWithLabel({ todo, setTodo, onClick }: Props): React.ReactNode {
+  return (
+    <>
+      <form className="grid w-full max-w-sm gap-1.5 mt-16 mx-auto mb-16">
+        <Label htmlFor="text">TODOOO</Label>
+        <div className="flex gap-2 relative">
+          <Input
+            type="text"
+            id="text"
+            placeholder="TODOOO"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <Button onClick={onClick} type="submit" className="absolute right-0">
+            Submit
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+interface Props2 {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+function ToDoCard({ todo, todos, setTodos }: Props2): React.ReactNode {
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleDone(id: number): void {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, isDone: true } : todo))
+    );
+  }
+
+  function handleDelete(id: number): void {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function handleEdit(e: React.FormEvent, id: number) {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setIsEdit(false);
+  }
+
+  useEffect(
+    function () {
+      inputRef.current?.focus();
+    },
+    [isEdit]
+  );
+
+  return (
+    <Card className="flex flex-col justify-between gap-8 w-72">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <CardTitle>Todo</CardTitle>
+        {todo.isDone && <Badge>Done</Badge>}
+      </CardHeader>
+      <CardContent>
+        {isEdit ? (
+          <form
+            className="flex flex-col gap-4 items-center justify-center"
+            onSubmit={(e) => handleEdit(e, todo.id)}
+          >
+            <Input
+              ref={inputRef}
+              placeholder="Edit here"
+              value={editTodo}
+              onChange={(e) => setEditTodo(e.target.value)}
+              className="py-14"
+            />
+            <Button className="w-1/3">done</Button>
+          </form>
+        ) : (
+          <p className="text-center">{todo.todo}</p>
+        )}
+      </CardContent>
+
+      <CardFooter className="flex flex-row gap-4 items-center justify-center">
+        {!todo.isDone && (
+          <>
+            {!isEdit && (
+              <Button
+                size="icon"
+                variant="default"
+                onClick={() => handleDone(todo.id)}
+              >
+                <Check size={15} />
+              </Button>
+            )}
+
+            <Button
+              size="icon"
+              variant="secondary"
+              onClick={() => {
+                if (!todo.isDone) {
+                  setIsEdit((isEdit) => !isEdit);
+                }
+              }}
+            >
+              <PenIcon size={15} />
+            </Button>
+            {!isEdit && (
+              <Button
+                size="icon"
+                variant="destructive"
+                onClick={() => handleDelete(todo.id)}
+              >
+                <Trash2 size={15} />
+              </Button>
+            )}
+          </>
+        )}
+      </CardFooter>
+    </Card>
+  );
 }
