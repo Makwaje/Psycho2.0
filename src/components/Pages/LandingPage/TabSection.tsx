252b86907404e8da9748246880c5dfeb495fdd43
makwaje <alloushi2010@gmail.com>
2023-11-25 09:50:37 +0200

    hi

--- src/components/Pages/LandingPage/TabSection.tsx
+++ src/components/Pages/LandingPage/TabSection.tsx
@@ -12,8 +12,8 @@ function TabSection() {
   return (
     <SectionContainer>
       <div className="h-auto bg-card">
-        <Tabs defaultValue="anxiety">
-          <TabsList className="flex h-auto flex-col items-start gap-16">
+        <Tabs defaultValue="anxiety" className="flex  flex-row gap-8">
+          <TabsList className="flex h-auto flex-col items-start gap-4">
             <TabsTrigger value="anxiety" className="w-44 p-6">
               Anxiety
             </TabsTrigger>
@@ -28,36 +28,46 @@ function TabSection() {
             </TabsTrigger>
           </TabsList>
 
-          <div className="h-auto">
-            <TabsContent value="anxiety">
-              <p className="w-full">
-                Anxiety is a feeling of worry, nervousness, or unease, typically
-                about an imminent event or something with an uncertain outcome -
-                It is a normal human emotion that everyone experiences from time
-                to time. However, if anxiety is severe or persistent, it can
-                interfere with daily life and may indicate an anxiety disorder.
-              </p>
-              <Image src="asset 1.svg" alt="anxiety" width={128} height={128} />
-            </TabsContent>
-            <TabsContent value="depression">
+          <TabsContent value="anxiety" className=" flex justify-around py-8">
+            <p className=" w-2/3 text-base">
+              Anxiety is a feeling of worry, nervousness, or unease, typically
+              about an imminent event or something with an uncertain outcome -
+              It is a normal human emotion that everyone experiences from time
+              to time. However, if anxiety is severe or persistent, it can
+              interfere with daily life and may indicate an anxiety disorder.
+            </p>
+            <Image src="asset 1.svg" alt="anxiety" width={128} height={128} />
+          </TabsContent>
+          <TabsContent
+            value="depression"
+            className="-ml-8 flex justify-around py-8"
+          >
+            <p className=" w-2/3 text-base">
               Depression is a common and serious medical illness that negatively
               affects how you feel, the way you think and how you act.
               Fortunately, it is also treatable. Depression causes feelings of
               sadness and/or a loss of interest in activities you once enjoyed.
               It can lead to a variety of emotional and physical problems and
               can decrease your ability to function at work and at home.
-              <Image src="asset 3.svg" alt="anxiety" width={128} height={128} />
-            </TabsContent>
-            <TabsContent value="pstd">
+            </p>
+            <Image src="asset 3.svg" alt="anxiety" width={128} height={128} />
+          </TabsContent>
+          <TabsContent value="pstd" className="-ml-8 flex justify-around py-8">
+            <p className=" w-2/3 text-base">
               Post-traumatic stress disorder (PTSD) is a mental disorder that
               can develop in people who have experienced or witnessed a
               traumatic event. A traumatic event is an event that is terrifying,
               shocking, or dangerous. Some examples of traumatic events include
               war, natural disasters, car accidents, sexual assault, and
               physical abuse.
-              <Image src="asset 2.svg" alt="anxiety" width={128} height={128} />
-            </TabsContent>
-            <TabsContent value="existential-stress">
+            </p>
+            <Image src="asset 2.svg" alt="anxiety" width={128} height={128} />
+          </TabsContent>
+          <TabsContent
+            value="existential-stress"
+            className="-ml-8 flex justify-around py-8 "
+          >
+            <p className=" w-2/3 text-base">
               Existential stress, also known as existential anxiety or dread, is
               a type of anxiety that arises from contemplating the meaning of
               life, death, and one&apos;s place in the universe. It can manifest
@@ -65,9 +75,9 @@ function TabSection() {
               purpose, and value. Unlike everyday anxieties or worries,
               existential stress stems from fundamental questions about the
               nature of reality and our place within it.
-              <Image src="asset 4.svg" alt="anxiety" width={128} height={128} />
-            </TabsContent>
-          </div>
+            </p>
+            <Image src="asset 4.svg" alt="anxiety" width={128} height={128} />
+          </TabsContent>
         </Tabs>
       </div>
     </SectionContainer>
