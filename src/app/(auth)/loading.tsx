f7ea21c8aa417e5d4400573724991b81fa18a0ae
makwaje <alloushi2010@gmail.com>
2023-12-10 09:52:06 +0200

    added loading spinner in auth page

--- src/app/(auth)/loading.tsx
+++ src/app/(auth)/loading.tsx
@@ -2,7 +2,7 @@ import { CgSpinner } from "react-icons/cg";
 
 function loading() {
   return (
-    <div className="flex h-full animate-spin items-center justify-center text-8xl font-extrabold">
+    <div className="relative  flex w-full animate-spin flex-col items-center justify-center text-8xl font-extrabold md:w-1/2">
       <CgSpinner />
     </div>
   );
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { CgSpinner } from "react-icons/cg";
 
 function loading() {
   return (
-    <div className="flex h-full animate-spin items-center justify-center text-8xl font-extrabold">
+    <div className="flex h-screen animate-spin items-center justify-center text-8xl font-extrabold">
       <CgSpinner />
     </div>
   );
