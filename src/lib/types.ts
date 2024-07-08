cbc5e8badbbf5f309028b9867d83042c48d07554
makwaje <alloushi2010@gmail.com>
2024-07-08 20:17:44 +0200

    uninstalled unused npm packages

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,6 +5,7 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(10, "Password must be at least 8 characters"),
 });
+
 export type TLoginSchema = z.infer<typeof loginSchema>;
 
 // signup schema validation to be used in react hook form
