86d1af94479b69fa6bc58698b7aa763f8182f7dd
makwaje <alloushi2010@gmail.com>
2024-07-13 23:59:41 +0200

    fix

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,8 +1,7 @@
 'use server';
 
-import { setCookie } from 'cookies-next';
-import { TLoginSchema, TSignUpSchema } from './types';
 import { cookies } from 'next/headers';
+import { TLoginSchema, TSignUpSchema } from './types';
 
 export async function signUp(formData: TSignUpSchema) {
   const res = await fetch(`${process.env.BASE_URL}/users/signup`, {
@@ -17,7 +16,7 @@ export async function signUp(formData: TSignUpSchema) {
     },
   });
   console.log(res);
-  getAndSetCookies(res);
+  // getAndSetCookies(res);
 
   return res.json();
 }
@@ -35,58 +34,58 @@ export async function logIn(formData: TLoginSchema) {
   });
 
   console.log(res);
-  getAndSetCookies(res);
+  // getAndSetCookies(res);
 
   return res.json();
 }
 
-function parseCookieString(cookieString: []) {
-  // Split the string by semicolons to separate cookies
-  const cookies = cookieString.split(';');
+// function parseCookieString(cookieString: []) {
+//   // Split the string by semicolons to separate cookies
+//   const cookies = cookieString.split(';');
 
-  // Create an empty object to store cookie data
-  const cookieObj = {};
+//   // Create an empty object to store cookie data
+//   const cookieObj = {};
 
-  for (const cookie of cookies) {
-    // Split each cookie by the equals sign to separate key and value
-    const [key, value] = cookie.trim().split('=');
+//   for (const cookie of cookies) {
+//     // Split each cookie by the equals sign to separate key and value
+//     const [key, value] = cookie.trim().split('=');
 
-    // Handle the special case of the JWT cookie
-    if (key === 'jwt') {
-      // Split the value further to separate token, expiration details
-      const [token, ...otherParts] = value.split(';');
-      cookieObj[key] = token;
+//     // Handle the special case of the JWT cookie
+//     if (key === 'jwt') {
+//       // Split the value further to separate token, expiration details
+//       const [token, ...otherParts] = value.split(';');
+//       cookieObj[key] = token;
 
-      // Parse expiration details (optional)
-      if (otherParts.length) {
-        const expirationObj = {};
-        for (const part of otherParts) {
-          const [subKey, subValue] = part.trim().split('=');
-          expirationObj[subKey] = subValue;
-        }
-        cookieObj.expiration = expirationObj;
-      }
-    } else {
-      // For other cookies, simply store key-value pair
-      cookieObj[key] = value;
-    }
-  }
+//       // Parse expiration details (optional)
+//       if (otherParts.length) {
+//         const expirationObj = {};
+//         for (const part of otherParts) {
+//           const [subKey, subValue] = part.trim().split('=');
+//           expirationObj[subKey] = subValue;
+//         }
+//         cookieObj.expiration = expirationObj;
+//       }
+//     } else {
+//       // For other cookies, simply store key-value pair
+//       cookieObj[key] = value;
+//     }
+//   }
 
-  return cookieObj;
-}
+//   return cookieObj;
+// }
 
-function getAndSetCookies(res: Response) {
-  const cookieString = res.headers.getSetCookie() as [];
+// function getAndSetCookies(res: Response) {
+//   const cookieString = res.headers.getSetCookie() as [];
 
-  const { jwt, Path } = parseCookieString(cookieString[0]);
-  console.log(jwt, Path);
+//   const { jwt, Path } = parseCookieString(cookieString[0]);
+//   console.log(jwt, Path);
 
-  const now = new Date();
-  const current = new Date(now.getFullYear(), now.getMonth() + 1);
+//   const now = new Date();
+//   const current = new Date(now.getFullYear(), now.getMonth() + 1);
 
-  cookies().set('jwt', jwt, {
-    expires: current,
-    path: Path,
-    httpOnly: true,
-  });
-}
+//   cookies().set('jwt', jwt, {
+//     expires: current,
+//     path: Path,
+//     httpOnly: true,
+//   });
+// }
