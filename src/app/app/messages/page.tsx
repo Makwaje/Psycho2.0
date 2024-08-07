f1902fbf832bbcc4c54c385d878f08d23e7c5c8b
makwaje <alloushi2010@gmail.com>
2024-08-07 16:01:26 +0200

    fix style issue

--- src/app/app/messages/page.tsx
+++ src/app/app/messages/page.tsx
@@ -7,9 +7,5 @@ export default function Messages() {
   const layout = cookies().get('react-resizable-panels:layout');
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
 
-  return (
-    <div>
-      <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
-    </div>
-  );
+  return <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />;
 }
